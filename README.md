0819f275b629c99d24f83283527ef35fa56bea6a
Anthony PERIQUET <anthony@periquet.net>
2015-09-03 11:43:45 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,105 @@
+# Vanilla framework
+
+Vanilla framework is a light library for DOM manipulation using Vanilla-JS
+
+## Installation
+
+Include `vanilla.min.js` script :
+```html
+<script src="vanilla.min.js" type="text/javascript" charset="utf-8" />
+```
+
+## Usage
+
+The framework use the `vanilla` namespace.<br />
+Examples below can be found in the [example page](./example/vanilla.html).
+
+### Create node and add content
+
+```html
+<div class="example"></div>
+```
+
+```js
+vanilla('<a>').attr('href', 'image1.jpg')
+    .append( vanilla('<img>').attr('src', 'thumbs/image1.jpg') )
+    .appendTo('.example:first-of-type');
+```
+
+### Adding overlay on item click
+
+```js
+// Define overlay
+overlay = vanilla('<div>')
+    .css('background', '#000')
+    .css('bottom', 0)
+    .css('display', 'none')
+    .css('height', '100%')
+    .css('left', 0)
+    .css('opacity', 0.75)
+    .css('position', 'fixed')
+    .css('right', 0)
+    .css('top', 0)
+    .css('width', '100%')
+    .css('z-index', 1)
+    .prependTo('body')
+    .on('click', function() {
+        overlay.fadeOut();
+    });
+
+// Add event handler
+vanilla('.example > a').on('click', function(event){
+    event.preventDefault();
+    overlay.fadeIn();
+});
+```
+
+## Available methods
+
+These are the currently supported methods:
+* attr
+* addClass
+* hasClass
+* removeClass
+* toggleClass
+* css
+* fadeIn
+* fadeOut
+* append
+* appendTo
+* prepend
+* prependTo
+* empty
+* html
+* prev
+* next
+* parent
+* each
+* ready
+* on
+* off
+
+# License (MIT)
+
+jQuery Modal is distributed under the [MIT License](Learn more at http://opensource.org/licenses/mit-license.php):
+
+    Copyright (c) 2015 Anthony PERIQUET
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ vanilla.js
@@ -0,0 +1,287 @@
+/**
+ * Vanilla Framework ;) (https://github.com/xylphid)
+ * Version 0.1.0
+ */
+var vanilla = (function(window, document) {
+    var events = []
+    var listeners = {};
+    var vanilla = function( query ){
+        if (!(this instanceof vanilla))
+            return new vanilla( query );
+
+        this.nodes = selector( query );
+        //this.fadeInit;
+    };
+
+    vanilla.prototype = {
+
+        // Attributes management
+        attr: function( attr, value ) {
+            if (typeof value != 'undefined') {
+                for (i = 0; i < this.nodes.length; i++) {
+                    this.nodes[i].setAttribute(attr, value);
+                }
+                return this;
+            }
+            else {
+                value = this.nodes[0].getAttribute(attr);
+                return value === null ? undefined : value;
+            }
+        },
+        // Class management
+        addClass: function( className ) {
+            classNames = className.split(' ');
+            for (i = 0; i < this.nodes.length; i++) {
+                for (key in classNames) {
+                    this.nodes[i].classList.add(classNames[key]);
+                }
+            }
+            return this;
+        },
+        
+        hasClass: function( className ) {
+            hasClass = false;
+            for (i = 0; i < this.nodes.length; i++) {
+                hasClass = hasClass || new RegExp(' ' + className + ' ').test(' ' + this.nodes[i].classList + ' ');
+            }
+            return hasClass;
+        },
+        
+        removeClass: function( className ) {
+            classNames = className.split(' ');
+            for (i = 0; i < this.nodes.length; i++) {
+                for (key in classNames) {
+                    this.nodes[i].classList.remove(classNames[key]);
+                }
+            }
+            return this;
+        },
+        
+        toggleClass: function( className ) {
+            for (i = 0; i < this.nodes.length; i++) {
+                this.nodes[i].classList.toggle( className );
+            }
+            return this;
+        },
+
+        // Style management
+        css: function( attr, value ) {
+            if (typeof value != 'undefined') {
+                for (i = 0; i < this.nodes.length; i++) {
+                    this.nodes[i].defaultStyle = typeof this.nodes[i].defaultStyle == typeof undefined ? {} : this.nodes[i].defaultStyle;
+                    if (typeof this.nodes[i].defaultStyle[ camelCase(attr) ] == typeof undefined) {
+                        this.nodes[i].defaultStyle[ camelCase(attr) ] = 
+                            this.nodes[i].style[ camelCase(attr) ] != '' ? this.nodes[i].style[ camelCase(attr) ] : value;
+                    }
+                    this.nodes[i].style[ camelCase(attr) ] = value;
+                }
+                return this;
+            } else { return this.nodes[0].style[camelCase(attr)]; }
+        },
+
+        fadeIn: function( duration ) {
+            duration = typeof duration == typeof undefined ? 500 : duration;
+            for (i = 0; i < this.nodes.length; i++) {
+                var s = this.nodes[i].style;
+                var opacity = typeof this.nodes[i].defaultStyle['opacity'] != typeof undefined ? this.nodes[i].defaultStyle['opacity'] : 1;
+                
+                s.opacity = 0;
+                s.display = "block";
+                (function unfade(){var val=parseFloat(s.opacity);if(!((val+=.01)>opacity)){s.opacity=val;setTimeout(unfade,duration/100);}})();
+            }
+        },
+
+        fadeOut: function( duration ) {
+            duration = typeof duration == typeof undefined ? 500 : duration;
+            for (i = 0; i < this.nodes.length; i++) {
+                var s = this.nodes[i].style;
+                s.opacity = s.opacity != null ? s.opacity : 1;
+                (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,duration/100)})();
+            }
+        },
+
+        // Interactions
+        append: function( elm ) {
+            for (i = 0; i < this.nodes.length; i++) {
+                for (j = 0; j < elm.nodes.length; j++) {
+                    this.nodes[i].appendChild( elm.nodes[j] );
+                }
+            }
+            return this;
+        },
+        appendTo: function( elm ) {
+            if (!(elm instanceof vanilla)) elm = new vanilla( elm );
+            for (i = 0; i < this.nodes.length; i++) {
+                for (j = 0; j < elm.nodes.length; j++) {
+                    elm.nodes[j].appendChild( this.nodes[i] );
+                }
+            }
+            return this;
+        },
+        prepend: function( elm ) {
+            for (i = 0; i < this.nodes.length; i++) {
+                first = this.nodes[i].hasChildNodes() ? this.nodes[i].firstChild : null;
+                for (j = 0; j < elm.nodes.length; j++) {
+                    first !== null ? this.nodes[i].insertBefore( elm.nodes[j], first ) : this.nodes[i].appendChild( elm.nodes[j] );
+                }
+            }
+            return this;
+        },
+        prependTo: function( elm ) {
+            if (!(elm instanceof vanilla)) elm = vanilla( elm );
+            for (i = 0; i < this.nodes.length; i++) {
+                for (j = 0; j < elm.nodes.length; j++) {
+                    first = elm.nodes[i].hasChildNodes() ? elm.nodes[i].firstChild : null;
+                    first !== null ? elm.nodes[i].insertBefore( this.nodes[j], first ) : elm.nodes[i].appendChild( this.nodes[j] );
+                }
+            }
+            return this;
+        },
+
+        empty: function() {
+            for (i = 0; i < this.nodes.length; i++) {
+                while (this.nodes[i].firstChild) { this.nodes[i].removeChild(this.nodes[i].firstChild); }
+            }
+        },
+
+        html: function( html ) {
+            if (typeof html != typeof undefined) {
+                for (i = 0; i < this.nodes.length; i++) { this.nodes[i].innerHTML = html; }
+                return this;
+            }
+            else
+                return this.nodes[0].innerHTML;
+        },
+
+        // Traversing
+        prev: function() {
+            sibling = this.nodes[0].previousSibling;
+            while (sibling !== null && (sibling.nodeType == 8 || (sibling.nodeType == 3 && !/\S/.test(sibling.nodeValue)))) {
+                sibling = sibling.previousSibling;
+            }
+
+            if (sibling !== null) {
+                o = new vanilla();
+                o.nodes = [sibling];
+            }
+            else o = sibling;
+            return o;
+        },
+
+        next: function() {
+            sibling = this.nodes[0].nextSibling;
+            while (sibling !== null && (sibling.nodeType == 8 || (sibling.nodeType == 3 && !/\S/.test(sibling.nodeValue)))) {
+                sibling = sibling.nextSibling;
+            }
+
+            if (sibling !== null) {
+                o = new vanilla();
+                o.nodes = [sibling];
+            }
+            else o = sibling;
+            return o;
+        },
+
+        parent: function() {
+            if (this.nodes[0].parentNode !== null) {
+                o = new vanilla();
+                o.nodes = [this.nodes[0].parentNode];
+            }
+            else o = this.nodes[0].parentNode;
+            return o;
+        },
+
+        each: function( handler ) {
+            if (typeof handler != 'function') return this;
+            var nodes = [];
+            for (var i = 0; i < this.nodes.length; i++) {
+                o = new vanilla();
+                o.nodes = [this.nodes[i]];
+                nodes.push(o);
+            }
+            [].forEach.call(nodes, handler);
+            return this;
+        },
+
+        // Events
+        ready: function( callback ) {
+            document.addEventListener('DOMContentLoaded', callback, false);
+        },
+
+        on: function(event, handler, capture) {
+            var nListeners = Object.keys(listeners).length
+            capture = typeof capture === typeof undefined ? false : capture;
+            // Add event listener
+            for (var i = 0; i < this.nodes.length; i++) {
+                this.nodes[i].addEventListener(event, handler, capture);
+                listeners[nListeners + i] = {
+                    element: this.nodes[i],
+                    event: event,
+                    handler: handler,
+                    capture: capture
+                };
+            }
+            return this;
+        },
+        off: function(event) {
+            var nListeners = Object.keys(listeners).length;
+            for (var i = 0; i < nListeners; i++) {
+                var rm = false;
+                for (var n = 0; n < this.nodes.length; n++) {
+                    var l = listeners[i];
+                    if (l.event = event) {
+                        this.nodes[n].removeEventListener(l.event, l.handler, l.capture);
+                        rm = true;
+                    }
+                }
+                if (rm === true) delete listeners[i];
+            }
+            return this;
+        }
+    };
+
+    var selector = function( query ) {
+        var elm = null;
+        if (typeof query !== 'string') {
+            // If it's not a string assume it's already an element 
+            elm = query;
+        } else if ( isTag(query) ) {
+            // check if it's a tag
+            var tag = query.substring(1, query.length-1);
+            elm = [document.createElement( tag )];
+        } else if ( isId(query) ) {
+            // check if it's an id
+            var id = query.substring(1);
+            elm = document.getElementById( id );
+        } else {
+            // run a basic query
+            elm = document.querySelectorAll( query );
+        }
+
+        return elm;
+    }
+
+    // utils
+    var camelCase = function( string ){
+        return  string.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    };
+
+    var isTag = function( string ){
+        return string.search(/^<\w+>$/) === 0;
+    };
+
+    var isId = function( string ){
+        return string.search(/^#\w+$/) === 0;
+    };
+
+    // return one item from the _selected stack
+    var getEl = function( i ){
+        i = i || 0;
+        // more sophisticated logic here?
+        return _selected[i] || _selected;
+    };
+
+    window.vanilla = new vanilla();
+    return vanilla;
+    
+}) (this.window, this.document);
